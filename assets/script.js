7de9e73080fa0e3b599cc22dc2f7ce6566b22a51
Anthony Pin <anthonypin0169@gmail.com>
2025-10-29 11:39:31 +0100

    Ajout des fleches et des bulletpoints

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,40 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//Creation des variables des fleches
+const arrow_left = document.querySelector('.arrow_left')
+console.log(arrow_left)
+const arrow_right = document.querySelector('.arrow_right')
+console.log(arrow_right)
+
+//Ajout de l'évenement au click
+let i = 0
+
+arrow_left.addEventListener('click', () => {
+		i--
+		console.log(i)
+	
+})
+
+arrow_right.addEventListener('click', () => {
+		i++
+		console.log(i)
+
+})
+
+//Creation des bullets points
+let dots = document.querySelector('.dots')
+
+let compteur = 0
+
+while(compteur < slides.length) {
+
+	let bulletPoint = document.createElement('div')
+	bulletPoint.classList.add('dot')
+	dots.appendChild(bulletPoint)
+
+	compteur++
+	console.log(compteur)
+
+}
